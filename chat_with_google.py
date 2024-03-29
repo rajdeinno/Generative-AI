392b7e356ef0db75212adf9ce934653e03737f62
Rajendran Tamilarasan <mail2rajdeinno@gmail.com>
2024-03-29 23:13:22 +0530

    Test changes commit

--- chat_with_google.py
+++ chat_with_google.py
@@ -10,6 +10,7 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 
+
 load_dotenv()
 os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
